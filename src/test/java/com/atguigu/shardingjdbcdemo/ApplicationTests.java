ccd9cd44b1abe28f39ff9a42c8b25405b90fe6e6
guo <guoxy0119@163.com>
2023-05-16 16:00:51 +0800

    水平分表

--- src/test/java/com/atguigu/shardingjdbcdemo/ApplicationTests.java
+++ src/test/java/com/atguigu/shardingjdbcdemo/ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.atguigu.shardingjdbcdemo;
 
+import com.atguigu.shardingjdbcdemo.entity.Course;
 import com.atguigu.shardingjdbcdemo.mapper.CourseMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +11,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class ApplicationTests {
+public class ApplicationTests {
 
     //注入mapper
     @Autowired
     private CourseMapper courseMapper;
 
+    //添加课程的方法
     @Test
-    void contextLoads() {
+    public void addCourse() {
+        for (int i = 0; i <=10; i++) {
+            Course course = new Course();
+            course.setCname("java"+i);
+            course.setUserId(100L);
+            course.setCstatus("Normal"+i);
+            courseMapper.insert(course);
+        }
+    }
 
+    //查询课程的方法
+    @Test
+    public void findCourse(){
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.eq("cid",865258802432180224L);
+        Course course = courseMapper.selectOne(wrapper);
+        System.out.println("查询出的课程为："+course);
     }
 
 }
